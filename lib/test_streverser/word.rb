a83b8e09830fb9420e5af2f79e457a23a813176d
andie5 <andieasa@googlemail.com>
2017-01-08 23:06:26 +0000

    added a input to word class - to execute code in pry

--- lib/test_streverser/word.rb
+++ lib/test_streverser/word.rb
@@ -1,8 +1,8 @@
 # lib/test_streverser/word.rb
 module TestStreverser
   class Word
-    def self.reversify
-      self.reverse
+    def self.reversify(input)
+      input.reverse
     end
   end
 end
