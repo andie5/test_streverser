1f9774ab9de9fbfb76bd521e116dbb54deceeaaa
andie5 <andieasa@googlemail.com>
2017-01-11 19:06:37 +0000

    updated require statements

--- lib/test_streverser.rb
+++ lib/test_streverser.rb
@@ -1,5 +1,5 @@
-require "test_streverser/version"
-require "test_streverser/word"
+require_relative "./test_streverser/version"
+require_relative "./test_streverser/word"
 
 module TestStreverser
   def self.hello
